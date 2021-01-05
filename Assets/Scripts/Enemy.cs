9cd2059394be6b8986dc9f88004bb236ad550316
cvr-software <cvrgaming@gmail.com>
2021-01-05 13:50:56 -0800

    Milestone 1.1 Completed
    Added Prototype files

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+    private GameObject player;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        
+        rb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10) Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab, powerupPrefab;
+
+    public int enemyCount;
+    public int waveNumber = 1;
+
+    private float spawnRange = 9.0f;
+    
+    void Start()
+    {
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+    }
+
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+            SpawnEnemyWave(waveNumber++);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosY = Random.Range(-spawnRange, spawnRange);
+        return new Vector3(spawnPosX, 0.0f, spawnPosY);
+    }
+}
